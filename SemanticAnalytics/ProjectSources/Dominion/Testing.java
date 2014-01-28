0c825b1ebe656e299ea0b5147842a43295605f9c
RReguillo <raul.reguillo@gmail.com>
2014-01-28 13:04:07 +0100

    Adding images in Dominion

--- SemanticAnalytics/ProjectSources/Dominion/Testing.java
+++ SemanticAnalytics/ProjectSources/Dominion/Testing.java
@@ -9,37 +9,19 @@ import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.Statement;
 import com.hp.hpl.jena.rdf.model.StmtIterator;
-
+import R.Dominion.*;
 public class Testing {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Rinvoker r = new Rinvoker(); 
 		
-		DQModel m; 
-		Operation oper; 
-		m=new DQModel("http://datos.gijon.es/doc/cultura-ocio/bibliotecas.rdf");
-		m.showModel();
-		oper = new Operation(m.getModel());
-	
-		//m.showModelWithFormat("TTL");
-		//System.out.println(oper.getAllProperties(m.getModel()).toString());
-		//System.out.println(oper.getNumberOfProperties(m.getModel()));
+		String data = "a<-rnorm(100); barplot(a); dev.off()";
 		
-		// Query
-		
-		String queryString ="PREFIX geo:<http://www.geonames.org/ontology#>" +
-				" PREFIX cal:<http://www.w3.org/2002/12/cal#> " +
-				" PREFIX dc:<http://purl.org/dc/terms/> " +
-				"SELECT ?place ?name ?direction ?description " +
-				"WHERE { " +
-				"?place a<http://www.geonames.org/ontology#Feature> . " +
-				"?place geo:name ?name . " +
- 				"?place cal:street-address ?direction . " +
-				"?place dc:description ?description" +
-				"}";
-		oper.queryModel(queryString);
-		// TODO
+		//r.getPlot("data(iris); attach(iris); plot(Sepal.Length, Petal.Length, col=unclass(Species)); dev.off()");
+		r.getPlot(data);
+		r.RinvokerClose();
 	}
 }
--- SemanticAnalytics/ProjectSources/R/Dominion/Rinvoker.java
+++ SemanticAnalytics/ProjectSources/R/Dominion/Rinvoker.java
@@ -1,21 +1,38 @@
 package R.Dominion;
 
+import java.awt.Frame;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import org.rosuda.REngine.REXP;
 import org.rosuda.REngine.REXPMismatchException;
+import org.rosuda.REngine.REngineException;
 import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
 
 public class Rinvoker {
 	private RConnection c;
-	// This could need a singleton
+	// TODO This could need a singleton
 	public Rinvoker(){
 		try {
 			c = new RConnection();
 		} catch (RserveException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
+	public void RinvokerClose(){
+		c.close();
+	}
+	// TODO --- Operations ----------------------------
 	public double simpleOperation(String data){
 		REXP x;
 		double result = Math.E; 
@@ -29,7 +46,44 @@ public class Rinvoker {
 		}
 		return result;
 	}
-	public void RinvokerClose(){
-		c.close();
+	//TODO give more options of return type (png, bmp, jpg) etc
+	/*
+	 * Options: 
+	 *  - Format
+	 *  - barplot, point plot, etc.
+	 */
+	public Image getPlot(String data){
+		Image img = null;
+		BufferedImage bi = null; 
+		REXP x; 
+		try {
+			x= c.parseAndEval("try(jpeg('test.jpg',quality=90))");
+			c.parseAndEval(data);
+			x = c.parseAndEval("r=readBin('test.jpg','raw',1024*1024); unlink('test.jpg'); r");
+			img = Toolkit.getDefaultToolkit().createImage(x.asBytes());
+			
+			bi = toBufferedImage(img); 
+			File outputfile = new File("./tempFiles/temp.jpg");
+			ImageIO.write(bi, "jpg", outputfile);
+
+		} catch (REngineException | REXPMismatchException | IOException e) {
+			e.printStackTrace();
+		} 
+		return img; 
 	}
+	private static BufferedImage toBufferedImage(Image src) {
+		// TODO calculates 
+        int w = src.getWidth(null);
+        int h = src.getHeight(null);
+        int type = BufferedImage.TYPE_INT_RGB;  // other options
+        if (w <= 0 || h <= 0){
+        	w = 450; 
+        	h = 450; 
+        }
+        BufferedImage dest = new BufferedImage(w, h, type);
+        Graphics2D g2 = dest.createGraphics();
+        g2.drawImage(src, 0, 0, null);
+        g2.dispose();
+        return dest;
+    }
 }
