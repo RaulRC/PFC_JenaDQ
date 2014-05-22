527e1414a6f4340bf0d224d940a99c97d2139f4d
RReguillo <raul.reguillo@gmail.com>
2014-05-22 18:39:28 +0200

    Initial Commit bitbucket.

---
+++ JenaDQ/src/Test/APISemDQ.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class APISemDQ {
+
+	public int publishContextualRule(){
+		return 1; 
+	}
+	public void getContextualRule(){
+		
+	}
+	public void updateContextualRule(){
+		
+	}
+	public void deleteContextualRule(){
+		
+	}
+}
